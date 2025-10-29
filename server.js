324d48bb8f659015b31faecbefe698f51da49b7a
Andrew17-2006 <andriy.veretelnyk@gmail.com>
2025-10-29 22:37:56 +0200

    fix: add GitHub Pages to CORS

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors({
   origin: [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
+    'https://andrew17-2006.github.io/loyalty-web-app-project-Node.js/'
   ],
   credentials: true,
   // Allow the custom header used as a username fallback
